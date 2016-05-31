127f3a6c29fa6a1a00d5dd3babd2bc4b5eaa972e
Jason C Fisher <jfisher@usgs.gov>
2016-05-31 07:55:50 -0700

    Add BSD license to code comments

--- R/GetSeasonalMult.R
+++ R/GetSeasonalMult.R
@@ -45,9 +45,33 @@ GetSeasonalMult <- function(x, reduction, d.in.mv.ave, tr.stress.periods) {
 }
 
 
-# The .ReduceAmp function was modified from 'Signal Amplitude Reduction fx'
-# authored by Darin McCoy [Copyright (c) 2011] and covered by the BSD License:
-# http://www.mathworks.com/matlabcentral/fileexchange/view_license?file_info_id=32409
+# The .ReduceAmp function was modified from 'Signal Amplitude Reduction fx'.
+#
+# Copyright (c) 2011 Darin McCoy
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 .ReduceAmp <- function(signal, reduction) {
   if (length(signal) == 0 || !is.numeric(signal))
     stop("Problem with 'signal' argument")
